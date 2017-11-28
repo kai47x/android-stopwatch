52677466535394e6788718b143eb33f1daeafc31
Kai47x <k.u.kietzke@gmail.com>
2017-11-28 22:05:45 +0100

    onResume gefixt

---
+++ GitPush.bat
@@ -0,0 +1,6 @@
+echo "little bugfix" >> README.md
+git init
+git add README.md
+git commit -m "seconds fixed"
+git remote add origin https://github.com/kai47x/Zellview3.git
+git push -u origin master
