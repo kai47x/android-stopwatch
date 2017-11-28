0512a8691aa55bbb1eb56e261f95a7b8033d0a2b
Kai47x <k.u.kietzke@gmail.com>
2017-11-28 22:04:12 +0100

    onResume gefixt

--- stopwatch-app/src/main/java/com/danielbostwick/stopwatch/ext/DurationExt.kt
+++ stopwatch-app/src/main/java/com/danielbostwick/stopwatch/ext/DurationExt.kt
@@ -3,17 +3,13 @@ package com.danielbostwick.stopwatch.ext
 import org.joda.time.Duration
 
 fun Duration.toTimeElapsedString(): String {
-    val hours = this.standardHours
-    val minutes = this.standardMinutes
     val seconds = this.standardSeconds % 60
+    val minutes = this.standardSeconds / 60
+    val hours = this.standardSeconds / 3600
 
-
-
-    //seconds = seconds % 60
-
-    val minuteStr = if (minutes > 10) "${minutes}" else "0${minutes}"
-    val secondStr = if (seconds > 10) "${seconds}" else "0${seconds}"
+    val minuteStr = if (minutes > 9) "${minutes}" else "0${minutes}"
+    val secondStr = if (seconds > 9) "${seconds}" else "0${seconds}"
 
     return if (hours > 0) "${hours}:${minuteStr}:${secondStr}"
-    else "${minuteStr}:${secondStr}"
+    else "E ${minuteStr}:${secondStr}"
 }
